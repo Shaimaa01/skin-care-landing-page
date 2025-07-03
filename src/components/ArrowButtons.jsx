a29a518213c88a5756c5bf22773b0adb7bc5fe37
Shaimaa01 <shisko939@gmail.com>
2025-07-03 14:20:22 +0200

    feat: Add tactile hover and active states to arrow  buttons

--- src/components/ArrowButtons.jsx
+++ src/components/ArrowButtons.jsx
@@ -2,19 +2,25 @@ import rightArrow from "/assets/icons/right arrow 1.svg";
 import leftArrow from "/assets/icons/left arrow 1.svg";
 
 const ArrowButtons = ({ onPrevClick, onNextClick, canPrev, canNext }) => {
+  const buttonBaseClasses = `
+    cursor-pointer w-[60px] h-[60px] lg:w-[80px] lg:h-[80px]  rounded-full flex justify-center items-center 
+    disabled:opacity-50 disabled:cursor-not-allowed
+    transform transition-transform duration-200 ease-in-out
+    hover:scale-105 active:scale-95
+  `;
   return (
     <div className="flex items-center justify-center gap-[50px] lg:gap-[80px]  mt-[40px] lg:mt-0 ">
       <button
         onClick={onPrevClick}
         disabled={!canPrev}
-        className=" cursor-pointer w-[60px] h-[60px] lg:w-[80px] lg:h-[80px] rounded-full border border-Juniper flex justify-center items-center hover:bg-Cream-100 transition-colors"
+        className={`${buttonBaseClasses}  border border-Juniper  hover:bg-Cream-100 transition-colors`}
       >
         <img src={leftArrow} alt="left Arrow icon" />
       </button>
       <button
         onClick={onNextClick}
         disabled={!canNext}
-        className=" cursor-pointer w-[60px] h-[60px] lg:w-[80px] lg:h-[80px] rounded-full bg-Juniper  flex justify-center items-center hover:opacity-80 transition-opacity"
+        className={`${buttonBaseClasses}  bg-Juniper  hover:opacity-80 transition-opacity`}
       >
         <img src={rightArrow} alt="right arrow icon" />
       </button>
